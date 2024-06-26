2aa85e9e1221cd6b259e37f7a0696237b62dd16f
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-06-26 19:15:23 +0100

    feat: new text input form component

---
+++ src/components/form/inputText.tsx
@@ -0,0 +1,36 @@
+import { InputProps } from "@/interface/form";
+import { cn } from "@/lib/utils";
+
+export default function InputText({
+    label,
+    name,
+    placeholder,
+    className,
+    disabled,
+    autocomplete,
+    ariaAutoComplete,
+    id,
+    handler,
+    type = "text",
+    value,
+}: InputProps) {
+    const identifier = id ? id : `${name}Id`;
+    return (
+        <>
+            {label && <label htmlFor={identifier}>{label}</label>}
+            <input
+                type={type}
+                onChange={handler}
+                value={value}
+                autoComplete={autocomplete}
+                aria-autocomplete={ariaAutoComplete}
+                disabled={disabled}
+                className={cn("", className)}
+                placeholder={placeholder}
+                aria-placeholder={placeholder}
+                name={name}
+                id={identifier}
+            />
+        </>
+    );
+}
